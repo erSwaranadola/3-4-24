e17cd8f98fa830697dd66fae6d7842da620a5733
erSwaranadola <er.Swaranadola@gmail.com>
2024-04-03 09:51:42 +0530

    Q1

---
+++ Q1.py
@@ -0,0 +1,4 @@
+def max_of_three(a, b, c):
+    return max(a, b, c)
+result = max_of_three(5, 10, 3)
+print(result)
---
+++ Q3.py
@@ -0,0 +1,6 @@
+def print_even_numbers(numbers):
+    for num in numbers:
+        if num % 2 == 0:
+            print(num)
+my_list = [1, 2, 3, 4, 5, 6,]
+print_even_numbers(my_list)
